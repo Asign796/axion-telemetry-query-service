18e62135435523402eea47e00ec17b0e461b9feb
an796 <anamika.singh510@gmail.com>
2026-08-27 23:22:35 +0530

    telemery update

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Settings:
     # Format: postgresql://<user>:<password>@<host>:<port>/<database>
     DATABASE_URL: str = os.getenv(
         "DATABASE_URL",
-        "postgresql://admin:admin%40123@postgres-service:5432/mydb",
+        "postgresql://admin:admin%40123@10.96.45.40:5432/mydb123",
     )
     # Allows configuring a different port, e.g., if we run multiple services
     PORT: int = int(os.getenv("PORT", "8000"))
